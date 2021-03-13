ec89eb48f331badde5fe5a53481aec5eb43028c9
Hannele Ruiz <justlemoncl@gmail.com>
2021-03-13 04:50:04 -0300

    Fixed source path for extra files

--- build.py
+++ build.py
@@ -16,7 +16,7 @@ def main():
 
     with open("source/base.json") as file:
         base = json.load(file)
-    with open(f"base_{sys.argv[1]}.json") as file:
+    with open(f"source/base_{sys.argv[1]}.json") as file:
         extra = json.load(file)
 
     base.update(extra)
