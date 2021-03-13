54710e86c501ba9df72c8156cfbbcd733c11f809
Hannele Ruiz <justlemoncl@gmail.com>
2021-03-13 05:17:03 -0300

    Added manifest.sii and versions.sii build support

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from pathlib import Path
 
 import sii
 
@@ -14,16 +15,28 @@ def main():
         print("Too Many arguments!")
         sys.exit(3)
 
+    Path("build/" + sys.argv[1] + "/universal/def").mkdir(parents=True, exist_ok=True)
+
     with open("source/base.json") as file:
-        base = json.load(file)
+        economy_base = json.load(file)
     with open(f"source/base_{sys.argv[1]}.json") as file:
         extra = json.load(file)
+    economy_base.update(extra)
+    economy = sii.from_dict(economy_base, "economy_data", "economy.data.storage")
+    with open("build/" + sys.argv[1] + "/universal/def/economy_data.sii", "w") as file:
+        file.write(economy)
 
-    base.update(extra)
+    with open("source/manifest.json") as file:
+        manifest_base = json.load(file)
+    manifest = sii.from_dict(manifest_base, "mod_package", ".universal")
+    with open("build/" + sys.argv[1] + "/universal/manifest.sii", "w") as file:
+        file.write(manifest)
 
-    string = sii.from_dict(base, "economy_data", "economy.data.storage")
-    with open("test.sii", "w") as file:
-        file.write(string)
+    with open("source/versions.json") as file:
+        versions_base = json.load(file)
+    versions = sii.from_dict(versions_base, "package_version_info", ".universal")
+    with open("build/" + sys.argv[1] + "/versions.sii", "w") as file:
+        file.write(versions)
 
 
 if __name__ == "__main__":
