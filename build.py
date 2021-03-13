99e43006485dc67ec1473d18980b1cdd08dfd664
Hannele Ruiz <justlemoncl@gmail.com>
2021-03-13 03:54:53 -0300

    Added Parsing and Writing to SII files

---
+++ build.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+import sii
+
+
+def main():
+    print(f"Arguments: {sys.argv}")
+
+    if len(sys.argv) > 2:
+        print("Too Much arguments!")
+        sys.exit(2)
+    elif len(sys.argv) < 2:
+        print("Too Many arguments!")
+        sys.exit(3)
+
+    with open("base.json") as file:
+        base = json.load(file)
+    with open(f"base_{sys.argv[1]}.json") as file:
+        extra = json.load(file)
+
+    base.update(extra)
+
+    string = sii.from_dict(base, "economy_data", "economy.data.storage")
+    with open("test.sii", "w") as file:
+        file.write(string)
+
+
+if __name__ == "__main__":
+    main()
---
+++ sii.py
@@ -0,0 +1,43 @@
+import re
+
+pattern = re.compile("([A-Za-z_]*)(@)?(tuple)?([0-9]*)?")
+
+
+def from_dict(contents: dict, stype: str, sname: str):
+    """
+    Dumps a SII-styled dictionary into an actually SII file.
+    """
+    string = "SiiNunit\n{\n    " + stype + " : " + sname + "\n    {\n"
+
+    for key, value in contents.items():
+        match = pattern.match(key)
+        groups = match.groups()
+        name = groups[0]
+
+        # Convert the item to the correct format
+        if isinstance(value, str):
+            val = f"\"{value}\""
+        elif "@" in groups and "tuple" in groups:
+            val = str(tuple(value))
+        else:
+            val = str(value)
+
+        if "@" in groups and groups[3].isnumeric():
+            print(f"B: Writing {name}[] as {val}")
+            string += "        " + groups[0] + "[]: " + val + "\n"
+        elif isinstance(value, list):
+            print(f"B: Writing {name} as {val}")
+            i = 0
+            for item in value:
+                if isinstance(item, list):
+                    item = tuple(item)
+                print(f"L: Writing {name}[{i}] as {item}")
+                string += "        " + groups[0] + f"[{i}]: " + str(item) + "\n"
+                i += 1
+        else:
+            print(f"B: Writing {name} as {val}")
+            string += "        " + groups[0] + ": " + val + "\n"
+
+    string += "    }\n}\n"
+
+    return string
