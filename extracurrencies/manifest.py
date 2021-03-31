a81b78b3e7d412bb91ad4c9b98de0633e10bc63b
Hannele Ruiz <justlemoncl@gmail.com>
2021-03-30 22:29:38 -0300

    Fixed AppVeyor environment variable name

--- extracurrencies/manifest.py
+++ extracurrencies/manifest.py
@@ -8,13 +8,13 @@ def compile_manifest(appveyor: bool):
     """
     Compiles the Manifest to a SII Compatible dict.
     """
-    if appveyor and "APPVEYOR_REPO_TAG_NAME" not in os.environ:
-        print(f"{Fore.RED}Error{Style.RESET_ALL}: Environment Variable APPVEYOR_REPO_TAG_NAME is not present!")
+    if appveyor and "APPVEYOR_BUILD_VERSION" not in os.environ:
+        print(f"{Fore.RED}Error{Style.RESET_ALL}: Environment Variable APPVEYOR_BUILD_VERSION is not present!")
         sys.exit(3)
 
     return {
         "display_name": "Additional Currencies for ETS2",
-        "package_version": os.environ["APPVEYOR_REPO_TAG_NAME"] if appveyor else "2.0-pre",
+        "package_version": os.environ["APPVEYOR_BUILD_VERSION"] if appveyor else "2.0-pre",
         "author": "Lemon (justalemon)",
         "category@0": "other",
         "icon": "icon.jpg",
